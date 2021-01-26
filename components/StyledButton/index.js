b2aa79873c20419d07fa1831b7fced70ac83ffab
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-01-26 22:39:14 +0530

    button styled

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -1,11 +1,17 @@
+import React from "react";
 import { View, Text, Pressable } from "react-native";
 import styles from "./styles";
 
 const StyledButton = () => {
    return (
-      <View>
-         <Pressable style={styles.button} onPress={console.warn("clicked")}>
-            <Text>tap</Text>
+      <View style={styles.container}>
+         <Pressable
+            style={styles.button}
+            onPress={() => {
+               console.warn("clicked");
+            }}
+         >
+            <Text style={styles.text}>tap</Text>
          </Pressable>
       </View>
    );
--- components/StyledButton/styles.js
+++ components/StyledButton/styles.js
@@ -1,7 +1,23 @@
-import StyleSheet from "react-native";
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-   button: {},
+   container: {
+      height: "100%",
+      width: "100%",
+      padding: 10,
+   },
+   text: {
+      fontSize: 12,
+      fontWeight: "500",
+      textTransform: "uppercase",
+   },
+   button: {
+      backgroundColor: "white",
+      height: 40,
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 20,
+   },
 });
 
 export default styles;
