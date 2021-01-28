ed2a31d1164f270dad1d5d1fbeaba0701662a57b
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-01-28 22:15:47 +0530

    header worked

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 
 import CarsList from "./components/CarsList";
+import Header from "./components/Header";
 export default function App() {
    return (
       <View style={styles.container}>
+         <Header />
          <CarsList />
          <StatusBar style="auto" />
       </View>
